6c5105f01ccbec9b6bc40e50772a7ebcd1bfe402
Rafał Wolak <RWolak@advaoptical.com>
2015-04-22 15:57:46 +0200

    P01 - find last specs

--- src/test/scala/com/robo/p99/scala/P01Spec.scala
+++ src/test/scala/com/robo/p99/scala/P01Spec.scala
@@ -1,10 +1,30 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import com.robo.p99.scala.P01._
 
 class P01Spec extends UnitSpec {
 
+  "last" should "return last element" in {
+    last(List(1, 1, 2, 3, 5, 8)) == 8
+  }
 
+
+  "last rec" should "return last element" in {
+    lastRecursive(List(1, 1, 2, 3, 5, 8)) == 8
+  }
+
+  "last" should "produce NoSuchElementException when last is invoked on empty List" in {
+    intercept[NoSuchElementException] {
+      lastRecursive(List.empty)
+    }
+  }
+
+  "last rec" should "produce NoSuchElementException when lastRecursive is invoked on empty List" in {
+    intercept[NoSuchElementException] {
+      last(List.empty)
+    }
+  }
 
 }
 
