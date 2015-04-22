bd49e1d0ab612397e8efe763ef13835c93c0ac92
Rafał Wolak <RWolak@advaoptical.com>
2015-04-22 16:40:37 +0200

    [P03] Find the Kth element of a list

--- src/main/scala/com/robo/p99/scala/P03.scala
+++ src/main/scala/com/robo/p99/scala/P03.scala
@@ -1,7 +1,23 @@
 package com.robo.p99.scala
 
+/**
+ * Find the Kth element of a list. By convention, the first element in the list is element 0.
+ *
+ * Example:
+    scala> nth(2, List(1, 1, 2, 3, 5, 8))
+    res0: Int = 2
+ */
 object P03 {
 
-  // TODO
+  def nth[T](k: Int, l: List[T]): T = {
+    l.apply(k)
+  }
+
+  /** * Recursive version **/
+  def nthRec[T](n: Int, l: List[T]): T = (n, l) match {
+    case (0, x :: xs) => x
+    case (_, Nil) => throw new IndexOutOfBoundsException("" + n)
+    case (k, x :: xs) => nthRec(k - 1, xs)
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P03Spec.scala
+++ src/test/scala/com/robo/p99/scala/P03Spec.scala
@@ -1,10 +1,37 @@
 package com.robo.p99.scala
 
+import P03._
 import org.p99.scala.UnitSpec
 
+/**
+ * Find the Kth element of a list. By convention, the first element in the list is element 0.
+ *
+ * Example:
+    scala> nth(2, List(1, 1, 2, 3, 5, 8))
+    res0: Int = 2
+ */
 class P03Spec extends UnitSpec {
 
-  // TODO
+  "nth" should "Find the Kth element of a list" in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) == 2
+  }
+
+  it should " throw IndexOutOfBounds" in {
+    intercept[IndexOutOfBoundsException] {
+      nth(2, List(1, 2))
+    }
+  }
+
+  "nthRec" should "Find the Kth element of a list" in {
+    nthRec(2, List(1, 1, 2, 3, 5, 8)) == 2
+  }
+
+  it should " throw IndexOutOfBounds" in {
+    intercept[IndexOutOfBoundsException] {
+      nthRec(2, List(1, 2))
+    }
+  }
+
 
 }
 
