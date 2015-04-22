23825b3361728a74ba28a89f945018080d897185
Rafał Wolak <RWolak@advaoptical.com>
2015-04-22 16:14:12 +0200

    [P02] Find the last but one element of a list

--- src/main/scala/com/robo/p99/scala/P02.scala
+++ src/main/scala/com/robo/p99/scala/P02.scala
@@ -2,6 +2,17 @@ package com.robo.p99.scala
 
 object P02 {
 
-  // TODO
+  /**
+   * Find the last but one element of a list.
+  Example:
+    scala> penultimate(List(1, 1, 2, 3, 5, 8))
+    res0: Int = 5
+   */
+  def penultimate[T](l: List[T]): T = {
+    if (l.size < 2)
+      throw new NoSuchElementException("penultimate element from list of size less than 2")
+    else
+      l.takeRight(2).head
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P02Spec.scala
+++ src/test/scala/com/robo/p99/scala/P02Spec.scala
@@ -1,10 +1,26 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import com.robo.p99.scala.P02._
 
+/**
+ * Find the last but one element of a list.
+  Example:
+    scala> penultimate(List(1, 1, 2, 3, 5, 8))
+    res0: Int = 5
+ */
 class P02Spec extends UnitSpec {
 
-  // TODO
+  "penultimate" should  "throw Exception when list is to small" in {
+    intercept[NoSuchElementException] {
+      penultimate(List("one element"))
+    }
+  }
+
+  it should "Find the last but one element of a list" in {
+    penultimate(List("one", "two")) == "two"
+    penultimate(List(1,2,2,1,3,4,5,6,7,8)) == 7
+  }
 
 }
 
