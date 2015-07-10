5a4ee0696cbe16d92811df8db3638d294b0bdfa0
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-10 23:08:02 +0200

    Problem 04 - Find the number of elements of a list

--- src/main/scala/com/robo/p99/scala/P04.scala
+++ src/main/scala/com/robo/p99/scala/P04.scala
@@ -1,7 +1,18 @@
 package com.robo.p99.scala
 
+/**
+ * Find the number of elements of a list.
+ */
 object P04 {
 
-  // TODO
+  def length[T](l: List[T]): Int = l.length
+
+  // recursive version
+  def lengthRec[T](l: List[T]): Int = {
+    l match {
+      case Nil => 0
+      case x :: xs => 1 + lengthRec(xs)
+    }
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P04Spec.scala
+++ src/test/scala/com/robo/p99/scala/P04Spec.scala
@@ -1,10 +1,28 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P04._
 
+/** Problem 04 - Find the number of elements of a list
+  *
+  *
+  */
 class P04Spec extends UnitSpec {
 
-  // TODO
+  "length" should "Find the number of elements of a list" in {
+    length(List(1,2,3,4)) === 4
+    lengthRec(List(1,2,3,4)) === 4
+  }
+
+  it should "return 0 for empty list" in {
+    length(List()) === 0
+    lengthRec(List()) === 0
+  }
+
+  it should "return 1 for one element list" in {
+    length(List(1)) === 1
+    lengthRec(List(1)) === 1
+  }
 
 }
 
