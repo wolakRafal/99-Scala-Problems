cd8ee517348ccf6770998b406a466549c7ebd702
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-11 19:52:14 +0200

    P09 (medium)   Pack consecutive duplicates of list elements into sublists.

--- src/main/scala/com/robo/p99/scala/P09.scala
+++ src/main/scala/com/robo/p99/scala/P09.scala
@@ -1,7 +1,29 @@
 package com.robo.p99.scala
 
+/**
+ * P09 (medium)
+  Pack consecutive duplicates of list elements into sublists.
+  If a list contains repeated elements they should be placed in separate sublists.
+
+Example:
+scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+ */
 object P09 {
 
-  // TODO
+  def pack[T](l: List[T]): List[List[T]] = {
+
+    def packRec(acc: List[List[T]], ll: List[T]): List[List[T]] = {
+      ll match {
+        case a@(x :: xs) => packRec(acc :+ a.takeWhile(x == _)  , a.dropWhile(x == _))
+        case Nil => acc
+      }
+    }
+  l match {
+    case Nil => List.empty[List[T]]
+    case list => packRec(List(List[T]()), l).tail
+  }
+
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P09Spec.scala
+++ src/test/scala/com/robo/p99/scala/P09Spec.scala
@@ -1,10 +1,20 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P09._
 
 class P09Spec extends UnitSpec {
 
-  // TODO
+  "pack" should "Pack consecutive duplicates of list elements into sublists" in {
+
+    assert(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ===
+      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+
+  it should "works also with empty lists" in {
+    assert(pack(List()) === List())
+  }
+
 
 }
 
