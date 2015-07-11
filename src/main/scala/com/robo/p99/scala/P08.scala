d4cdc375ee05e6e4058ae20ea4a41488b9d3d222
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-11 19:30:41 +0200

    P08 (medium)   Eliminate consecutive duplicates of list elements.   If a list contains repeated elements they should be replaced with a single copy of the element.   The order of the elements should not be changed.

--- src/main/scala/com/robo/p99/scala/P08.scala
+++ src/main/scala/com/robo/p99/scala/P08.scala
@@ -1,7 +1,19 @@
 package com.robo.p99.scala
 
+/**
+ * P08 (medium)
+  Eliminate consecutive duplicates of list elements.
+  If a list contains repeated elements they should be replaced with a single copy of the element.
+  The order of the elements should not be changed.
+
+Example:
+scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+ */
 object P08 {
 
-  // TODO
+  def compress[T](l : List[T]) : List[T] = {
+    l.foldLeft(List.empty[T])((acc, a) => if (acc.contains(a)) acc else acc :+ a )
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P08Spec.scala
+++ src/test/scala/com/robo/p99/scala/P08Spec.scala
@@ -1,10 +1,26 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P08._
+/**
+ * P08 (medium)
+  Eliminate consecutive duplicates of list elements.
+  If a list contains repeated elements they should be replaced with a single copy of the element.
+  The order of the elements should not be changed.
 
+Example:
+scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+ */
 class P08Spec extends UnitSpec {
 
-  // TODO
+  "compress" should "Eliminate consecutive duplicates of list elements. " in {
+    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List('a, 'b, 'c, 'a, 'd, 'e)
+  }
+
+  it should "work with empty list" in {
+    compress(List()) === List()
+  }
 
 }
 
