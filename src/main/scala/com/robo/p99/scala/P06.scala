35e8ae1b1a761e7fad21aade8d34fedc65f3e185
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-11 18:58:16 +0200

    P06 (easy)   Find out whether a list is a palindrome.

--- src/main/scala/com/robo/p99/scala/P06.scala
+++ src/main/scala/com/robo/p99/scala/P06.scala
@@ -1,7 +1,18 @@
 package com.robo.p99.scala
 
+/**
+ * P06 (easy)
+  Find out whether a list is a palindrome.
+  Example:
+
+scala> isPalindrome(List(1, 2, 3, 2, 1))
+res0: Boolean = true
+ */
 object P06 {
 
-  // TODO
+  def isPalindrome[T](l: List[T]): Boolean = {
+    l == l.reverse
+  }
+
 
 }
--- src/test/scala/com/robo/p99/scala/P06Spec.scala
+++ src/test/scala/com/robo/p99/scala/P06Spec.scala
@@ -2,6 +2,14 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P06 (easy)
+Find out whether a list is a palindrome.
+Example:
+
+scala> isPalindrome(List(1, 2, 3, 2, 1))
+res0: Boolean = true
+ */
 class P06Spec extends UnitSpec {
 
   // TODO
