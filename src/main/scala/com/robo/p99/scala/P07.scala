02995b6a18f334b8eb1ae6cb4fe1257939a72935
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-11 19:09:09 +0200

    P07 (medium) Flatten a nested list structure.
    Example:
    scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
    res0: List[Any] = List(1, 1, 2, 3, 5, 8)

--- src/main/scala/com/robo/p99/scala/P07.scala
+++ src/main/scala/com/robo/p99/scala/P07.scala
@@ -1,7 +1,22 @@
 package com.robo.p99.scala
 
+/**
+ * P07 (medium)
+Flatten a nested list structure.
+
+Example:
+scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+ */
 object P07 {
 
-  // TODO
+  def flatten[T](l: List[List[T]]) = {
+    l.flatten
+  }
+
+
+  def flatten2[T](l: List[List[T]]) = {
+    l.foldRight(List.empty[T])(_ ++ _)
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P07Spec.scala
+++ src/test/scala/com/robo/p99/scala/P07Spec.scala
@@ -1,10 +1,33 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P07._
+/**
+ * P07 (medium)
+Flatten a nested list structure.
 
+Example:
+scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+ */
 class P07Spec extends UnitSpec {
 
-  // TODO
+  "flatten" should "Flatten a nested list structure." in {
+    flatten(List(List(1,2,3), List(4,5), List(6))) === List(1,2,3,4,5,6)
+  }
+
+  it should "works with empty list" in {
+    flatten(List()) === List
+  }
+
+  "flatten2" should "Flatten a nested list structure." in {
+    flatten2(List(List(1,2,3), List(4,5), List(6))) === List(1,2,3,4,5,6)
+  }
+
+  it should "works with empty list" in {
+    flatten2(List()) === List
+  }
+
 
 }
 
