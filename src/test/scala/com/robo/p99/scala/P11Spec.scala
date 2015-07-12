94fff65a9626885e5cdaf063a57de67bac1b3f4f
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-12 22:59:34 +0200

    P11 (easy)         Modified run-length encoding.

--- src/test/scala/com/robo/p99/scala/P11Spec.scala
+++ src/test/scala/com/robo/p99/scala/P11Spec.scala
@@ -4,7 +4,10 @@ import org.p99.scala.UnitSpec
 
 class P11Spec extends UnitSpec {
 
-  // TODO
+  "encodeModified" should "Modified run-length encoding. "  in {
+    assert(P11.encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==
+                              List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+  }
 
 }
 
