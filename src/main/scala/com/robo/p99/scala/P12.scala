8348faa90ac42e48fa92ec337605c916f158618a
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-12 23:05:58 +0200

    P12 (easy)       Decode a run-length encoded list.

--- src/main/scala/com/robo/p99/scala/P12.scala
+++ src/main/scala/com/robo/p99/scala/P12.scala
@@ -1,7 +1,20 @@
 package com.robo.p99.scala
 
+/**
+ * P12 (medium)
+  Decode a run-length encoded list.
+
+  Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+
+Example:
+scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
+ */
 object P12 {
 
-  // TODO
+  def decode[A](l: List[(Int, A)]): List[A] = {
+    l.foldLeft(List.empty[A])((acc, t) => acc ++ List.fill(t._1)(t._2))
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P12Spec.scala
+++ src/test/scala/com/robo/p99/scala/P12Spec.scala
@@ -2,9 +2,23 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P12 (medium)
+  Decode a run-length encoded list.
+
+  Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+
+Example:
+scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+*/
 class P12Spec extends UnitSpec {
 
-  // TODO
+  "decode" should "Decode a run-length encoded list." in {
+    assert(P12.decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) ===
+                      List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  }
+
 
 }
 
