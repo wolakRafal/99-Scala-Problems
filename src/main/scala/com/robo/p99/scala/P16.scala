d95012bde0011d2b0f878826c6a5c3b51a34c421
Rafal Wolak <wolak.rafal@gmail.com>
2015-07-18 14:02:53 +0200

    P16: Split a list into two parts. The length of the first part is given. Use a Tuple for your result.

--- src/main/scala/com/robo/p99/scala/P16.scala
+++ src/main/scala/com/robo/p99/scala/P16.scala
@@ -1,7 +1,24 @@
 package com.robo.p99.scala
 
+/**
+ * P17 (easy)
+  Split a list into two parts. The length of the first part is given. Use a Tuple for your result.
+
+Example:
+
+scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+ */
 object P16 {
 
-  // TODO
+  def split[A](n: Int, l: List[A]): (List[A], List[A]) = l.splitAt(n)
+  def split2[A](n: Int, l: List[A]): (List[A], List[A]) = (l.take(n), l.drop(n))
+
+  /* recursive version */
+  def splitRec[A](n: Int, la: List[A], acc:(List[A], List[A]) = (List(), List())): (List[A], List[A]) = (n, la) match {
+    case (0, l) =>      (acc._1, l)
+    case (_, List()) => acc
+    case (k, x :: xs) => splitRec(k - 1, xs, (acc._1 :+ x, xs))
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P16Spec.scala
+++ src/test/scala/com/robo/p99/scala/P16Spec.scala
@@ -1,10 +1,60 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P16._
 
+/**
+ * P17 (easy)
+  Split a list into two parts. The length of the first part is given. Use a Tuple for your result.
+
+Example:
+
+scala> split(N, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+ */
 class P16Spec extends UnitSpec {
 
-  // TODO
+  val in = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+  val out = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  val N = 3
+  
+  "split" should "Split a list into two parts" in {
+    assert(split(N, in) === out)
+  }
+
+  "split2" should "Split a list into two parts" in {
+    assert(split2(N, in) === out)
+  }
+
+  "splitRec" should "Split a list into two parts" in {
+    assert(splitRec(N, in) === out)
+  }
+
+  // edge cases
+
+  "split" should "work with n==0" in {
+    assert(split(0, in) === (List(), in))
+  }
+
+  "split2" should "work with n==0" in {
+    assert(split2(0, in) === (List(), in))
+  }
+
+  "splitRec" should "work with n==0" in {
+    assert(splitRec(0, in)  === (List(), in))
+  }
+
+  "split" should "Split a empty list into two empty list" in {
+    assert(split(N, List())  === (List(), List()))
+  }
+
+  "split2" should "Split a empty list into two empty list" in {
+    assert(split2(N, List()) === (List(), List()))
+  }
+
+  "splitRec" should "Split a empty list into two empty list" in {
+    assert(splitRec(N, List()) === (List(), List()))
+  }
 
 }
 
