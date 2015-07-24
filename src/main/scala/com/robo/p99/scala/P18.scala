6c421c0081c2943c0942d5bb190d28e56d27ae94
Rafał Wolak <RWolak@advaoptical.com>
2015-07-24 19:30:35 +0200

    [18] Extract a slice from a list

--- src/main/scala/com/robo/p99/scala/P18.scala
+++ src/main/scala/com/robo/p99/scala/P18.scala
@@ -1,7 +1,16 @@
 package com.robo.p99.scala
 
+/**
+ * P18 (medium)
+Extract a slice from a list. Given two indices, I and K, the slice is the list containing the elements from and including the Ith element up to but not
+including the Kth element of the original list. Start counting the elements with 0.
+
+Example:
+scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: List[Symbol] = List('d, 'e, 'f, 'g)
+ */
 object P18 {
 
-  // TODO
+  def slice[A](i: Int, k: Int, list: List[A]): List[A] = list.slice(i, k)
 
 }
--- src/test/scala/com/robo/p99/scala/P18Spec.scala
+++ src/test/scala/com/robo/p99/scala/P18Spec.scala
@@ -1,10 +1,33 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P18._
 
+/**
+ *
+scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: List[Symbol] = List('d, 'e, 'f, 'g)
+ */
 class P18Spec extends UnitSpec {
 
-  // TODO
+  "slice" should "Extract a slice from a list" in {
+    assert(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List('d, 'e, 'f, 'g))
+  }
+
+
+  "slice(0, 0, list)" should "Extract a empty from a list" in {
+    assert(slice(0, 0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List())
+  }
+
+  "slice from empty list " should "throw exception" in {
+    try {
+      slice(0, 0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      assert(false, "should throw exception")
+    }
+    catch {
+      case t: Throwable => assert(true)
+    }
+  }
 
 }
 
