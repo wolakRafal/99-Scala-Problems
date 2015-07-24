6431bb8368484f51b971d1f5c4694b2d8e1259e7
Rafał Wolak <RWolak@advaoptical.com>
2015-07-24 22:38:27 +0200

    P20 (easy): Remove the Kth element from a list. Return the list and the removed element in a Tuple. Elements are numbered from 0.

--- src/main/scala/com/robo/p99/scala/P20.scala
+++ src/main/scala/com/robo/p99/scala/P20.scala
@@ -1,7 +1,35 @@
 package com.robo.p99.scala
 
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * P20 (easy)
+Remove the Kth element from a list. Return the list and the removed element in a Tuple. Elements are numbered from 0.
+Example:
+
+scala> removeAt(1, List('a, 'b, 'c, 'd))
+res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+ */
 object P20 {
 
-  // TODO
+  def removeAt[A](k: Int, l: List[A]): (List[A], A) = {
+    val a = new ArrayBuffer[A](l.size)
+    a.++=(l)
+    val removed = a.remove(k)
+    (a.toList, removed)
+  }
+
+  // much faster tail recursive version
+  def removeAtRecursive[A](k: Int, l: List[A]): (List[A], A) = {
+    def removeAtRec(n: Int, toGo: List[A], processed: List[A]): (List[A], A) = {
+      (n, toGo) match {
+        case (0, x::xs) => (processed ++ xs, x)
+        case (_, Nil) => throw new IllegalArgumentException
+        case (m, x::xs) => removeAtRec(m - 1, xs, x::processed)
+      }
+    }
+
+    if (k < l.size) removeAtRec(k,l,List.empty[A]) else throw new IllegalArgumentException
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P20Spec.scala
+++ src/test/scala/com/robo/p99/scala/P20Spec.scala
@@ -1,10 +1,60 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P20._
+/**
+ * P20 (easy)
+Remove the Kth element from a list. Return the list and the removed element in a Tuple. Elements are numbered from 0.
+Example:
 
+scala> removeAt(1, List('a, 'b, 'c, 'd))
+res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+ */
 class P20Spec extends UnitSpec {
 
-  // TODO
+  val longList = List.fill(100000)("12")
+  val N : Int=  longList.size - 1 // / 2
+
+  "removeAt" should "Remove the Kth element from a list" in {
+    assert(removeAt(1, List('a, 'b, 'c, 'd)) === (List('a, 'c, 'd),'b))
+  }
+
+  it should "throw IllegalArgumentException when k is < 0" in {
+    intercept[Exception](removeAt(-1, List('a, 'b, 'c, 'd)))
+  }
+
+  it should "throw IllegalArgumentException when k is > list.size" in {
+    intercept[Exception](removeAt(12, List('a, 'b, 'c, 'd)))
+  }
+
+  it should " test performance" in {
+    val t = System.currentTimeMillis()
+    1 to 100 foreach( _ => removeAt(N, longList))
+    val stop = System.currentTimeMillis() - t
+    println("RemoveAt time:" + stop)
+    assert(true)
+  }
+
+  "removeAtRecursive" should "Remove the Kth element from a list" in {
+    assert(removeAtRecursive(1, List('a, 'b, 'c, 'd)) === (List('a, 'c, 'd),'b))
+  }
+
+  it should "throw IllegalArgumentException when k is < 0" in {
+    intercept[Exception](removeAtRecursive(-1, List('a, 'b, 'c, 'd)))
+  }
+
+  it should "throw IllegalArgumentException when k is > list.size" in {
+    intercept[Exception](removeAtRecursive(12, List('a, 'b, 'c, 'd)))
+  }
+
+  it should " test performance" in {
+    val t = System.currentTimeMillis()
+    1 to 100 foreach( _ => removeAtRecursive(N, longList))
+    val stop = System.currentTimeMillis() - t
+    println("RemoveAtRecursive time:" + stop)
+    assert(true)
+  }
+
 
 }
 
