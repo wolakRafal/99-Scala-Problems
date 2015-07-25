3e220ef4f9eb7e41e2336142abd93bcfa0ab304c
Rafał Wolak <RWolak@advaoptical.com>
2015-07-25 09:48:34 +0200

    P26 (medium): Generate the combinations of K distinct objects chosen from the N elements of a list

--- src/main/scala/com/robo/p99/scala/P26.scala
+++ src/main/scala/com/robo/p99/scala/P26.scala
@@ -1,7 +1,18 @@
 package com.robo.p99.scala
 
+/**
+ * P26 (medium)
+Generate the combinations of K distinct objects chosen from the N elements of a list.
+In how many ways can a committee of 3 be chosen from a group of 12 people?
+We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficient).
+For pure mathematicians, this result may be great. But we want to really generate all the possibilities.
+
+Example:
+scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+ */
 object P26 {
 
-  // TODO
+  def combinations[T](k: Int, l: List[T]): List[List[T]] = l.combinations(k).toList
 
 }
--- src/test/scala/com/robo/p99/scala/P26Spec.scala
+++ src/test/scala/com/robo/p99/scala/P26Spec.scala
@@ -2,9 +2,22 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P26 (medium)
+Generate the combinations of K distinct objects chosen from the N elements of a list.
+In how many ways can a committee of 3 be chosen from a group of 12 people?
+We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficient).
+For pure mathematicians, this result may be great. But we want to really generate all the possibilities.
+
+Example:
+scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+ */
 class P26Spec extends UnitSpec {
 
-  // TODO
+  "combination" should "Generate 220 combinations of 3 distinct objects chosen from the 12 elements of a list" in {
+    assert(P26.combinations(3, List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)).size == 220)
+  }
 
 }
 
