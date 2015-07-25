ae42e8a3704758c2e54e605983196db34770ae3f
Rafał Wolak <RWolak@advaoptical.com>
2015-07-25 09:37:09 +0200

    P24 (easy) Lotto: Draw N different random numbers from the set 1..M.

--- src/main/scala/com/robo/p99/scala/P24.scala
+++ src/main/scala/com/robo/p99/scala/P24.scala
@@ -1,7 +1,19 @@
 package com.robo.p99.scala
 
+import scala.util.Random
+
+/**
+ * P24 (easy)
+Lotto: Draw N different random numbers from the set 1..M.
+
+Example:
+scala> lotto(6, 49)
+res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+ */
 object P24 {
 
-  // TODO
+  def lotto(n: Int, m: Int): List[Int] = {
+    Random.shuffle(1 to m).take(n).toList
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P24Spec.scala
+++ src/test/scala/com/robo/p99/scala/P24Spec.scala
@@ -1,10 +1,35 @@
 package com.robo.p99.scala
 
+import com.robo.p99.scala.P24._
 import org.p99.scala.UnitSpec
 
+/**
+ * P24 (easy)
+Lotto: Draw N different random numbers from the set 1..M.
+
+Example:
+scala> lotto(6, 49)
+res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+*/
 class P24Spec extends UnitSpec {
 
-  // TODO
+  "randomSelect" should "Extract a given number of randomly selected elements from a list." in {
+    val l  = lotto(6, 49)
+    assert(l.size === 6, "size should match the n")
+    1 to 10000 foreach {_=>
+      assert(l.size == l.distinct.size, "list should have distinct values")
+    }
+  }
+
+  it should "return empty list when n=0 ." in {
+    assert(lotto(0, 49) === List())
+  }
+
+
+  it should "return all list l when n>l.size ." in {
+    assert(lotto(110, 49).size === 49)
+  }
+
 
 }
 
