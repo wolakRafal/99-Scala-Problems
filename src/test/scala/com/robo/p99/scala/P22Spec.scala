4b6ec757d6171ab7ef3262c669cc3391b33e8f4c
Rafał Wolak <RWolak@advaoptical.com>
2015-07-25 08:58:01 +0200

    P22 (easy): range. So ease that I omit tests.

--- src/test/scala/com/robo/p99/scala/P22Spec.scala
+++ src/test/scala/com/robo/p99/scala/P22Spec.scala
@@ -2,9 +2,15 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**P22 (easy)
+Create a list containing all integers within a given range.
+Example:
+scala> range(4, 9)
+res0: List[Int] = List(4, 5, 6, 7, 8, 9)
+  */
 class P22Spec extends UnitSpec {
 
-  // TODO
+  def range(start: Int, end: Int): List[Int] = (start to end).toList
 
 }
 
