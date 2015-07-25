90e12f02e829908f18326b2cab652cd49d5d8580
Rafał Wolak <RWolak@advaoptical.com>
2015-07-25 09:20:33 +0200

    P23 (medium) Extract a given number of randomly selected elements from a list.

--- src/main/scala/com/robo/p99/scala/P23.scala
+++ src/main/scala/com/robo/p99/scala/P23.scala
@@ -1,7 +1,30 @@
 package com.robo.p99.scala
 
+import scala.util.Random
+
+/**
+ * P23 (medium)
+Extract a given number of randomly selected elements from a list.
+
+Example:
+scala> randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+res0: List[Symbol] = List('e, 'd, 'a)
+
+Hint: Use the solution to problem P20
+ */
 object P23 {
 
-  // TODO
+  def randomSelect[T](n: Int, l: List[T]): List[T] = {
+    var tmpList = l
+    var resultList = List.empty[T]
+    1 to n foreach { _ =>
+      val idx = Random.nextInt(tmpList.size - 1)
+      val (ll, elem) = P20.removeAtRecursive(idx, tmpList)
+      tmpList = ll
+      resultList = elem :: resultList
+    }
+    resultList
+  }
+
 
 }
--- src/test/scala/com/robo/p99/scala/P23Spec.scala
+++ src/test/scala/com/robo/p99/scala/P23Spec.scala
@@ -1,10 +1,33 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P23._
 
+/**
+ * P23 (medium)
+Extract a given number of randomly selected elements from a list.
+
+Example:
+scala>
+
+Hint: Use the solution to problem P20
+ */
 class P23Spec extends UnitSpec {
 
-  // TODO
+  "randomSelect" should "Extract a given number of randomly selected elements from a list." in {
+    val l  = randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+    assert(l.size === 3, "size should match the n")
+    assert(l.size == l.distinct.size, "list should have distinct values")
+  }
+
+  it should "return empty list when n=0 ." in {
+    assert(randomSelect(0, List('a, 'b, 'c, 'd, 'f, 'g, 'h)) === List())
+  }
+
+
+  it should "throw exception when n>l.size ." in {
+    intercept[Exception](randomSelect(110, List('a, 'b, 'c, 'd, 'f, 'g, 'h)))
+  }
 
 }
 
