0e0cd37b5ec7e1b2853941c64e290eedd0d3f2e0
Rafał Wolak <RWolak@advaoptical.com>
2015-07-25 09:40:28 +0200

    P25 (easy) Generate random permutation of the elements of the list

--- src/main/scala/com/robo/p99/scala/P25.scala
+++ src/main/scala/com/robo/p99/scala/P25.scala
@@ -1,7 +1,16 @@
 package com.robo.p99.scala
 
+/**
+ * P25 (easy)
+Generate a random permutation of the elements of a list.
+
+Example:
+scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+Hint: Use the solution of problem P23.
+ */
 object P25 {
 
-  // TODO
+  def randomPermute[T](l: List[T]): List[T] = P23.randomSelect(l.size, l)
 
 }
