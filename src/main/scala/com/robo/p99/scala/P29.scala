3de0680a273896e87553df690ef71884c78e8125
Rafał Wolak <RWolak@advaoptical.com>
2015-07-27 11:35:55 +0200

    P29 (medium): Sorting a list of lists according to length of sublists.

--- src/main/scala/com/robo/p99/scala/P29.scala
+++ src/main/scala/com/robo/p99/scala/P29.scala
@@ -1,7 +1,17 @@
 package com.robo.p99.scala
 
+/**
+ * P29 (medium)
+  Sorting a list of lists according to length of sublists.
+  We suppose that a list contains elements that are lists themselves.
+  The objective is to sort the elements of the list according to their length. E.g. short lists first, longer lists later, or vice versa.
+
+Example:
+scala> lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
+res0: List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
+ */
 object P29 {
 
-  // TODO
+  def lsort[T](l: List[List[T]]): List[List[T]] = l.sortBy(_.size)
 
 }
--- src/test/scala/com/robo/p99/scala/P29Spec.scala
+++ src/test/scala/com/robo/p99/scala/P29Spec.scala
@@ -2,9 +2,37 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+
+/**
+ * P29 (medium)
+Sorting a list of lists according to length of sublists.
+We suppose that a list contains elements that are lists themselves.
+The objective is to sort the elements of the list according to their length. E.g. short lists first, longer lists later, or vice versa.
+
+Example:
+scala> lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
+res0: List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
+ */
 class P29Spec extends UnitSpec {
 
-  // TODO
+  "lsort" should "Sorting a list of lists according to length of sublists." in {
+    assert(P29.lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) ===
+                     List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l)))
+  }
 
+  it should "preserve ordering " in {
+    val l = List(List('a, 'b, 'c), List('f, 'd, 'e), List('f, 'g, 'h), List('a, 'a, 'a))
+    assert(P29.lsort(l) === l)
+  }
+
+  it should "work with empty list" in {
+    val l = List()
+    assert(P29.lsort(l) === l)
+  }
+
+  it should "sort empty lists" in {
+    val l = List(List())
+    assert(P29.lsort(l) === l)
+  }
 }
 
