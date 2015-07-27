942f32a707045f7c3bf0c753d00039abb723def2
Rafał Wolak <RWolak@advaoptical.com>
2015-07-27 11:29:29 +0200

    P28 (medium): Generalize the predicate from P27 in a way that we can specify a list of group sizes and the predicate will return a list of groups.

--- src/main/scala/com/robo/p99/scala/P28.scala
+++ src/main/scala/com/robo/p99/scala/P28.scala
@@ -1,7 +1,62 @@
 package com.robo.p99.scala
 
+/**
+ * P28 (medium)
+You need to do P27 first.
+
+Generalize the predicate from P27 in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+
+Example:
+scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo, Ida)), ...
+
+Note that we do not want permutations of the group members; i.e. ((Aldo, Beat), ...) is the same solution as ((Beat, Aldo), ...).
+However, we make a difference between ((Aldo, Beat), (Carla, David), ...) and ((Carla, David), (Aldo, Beat), ...).
+You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
+
+ */
 object P28 {
 
-  // TODO
+  type Combination[E] = List[E]
+
+  /** Quite elegant recursive implementation */
+  def group[T](groupsCount: List[Int], list: List[T]): List[List[Combination[T]]] = {
+    groupsCount match {
+      case Nil =>
+        List(List.empty[List[T]])
+      case n :: ns =>
+        val combAndRestList: List[(Combination[T], List[T])] = takeCombinations(n, list)
+
+        (for {
+          (combination, remaining) <- combAndRestList
+        } yield combine(combination, group(ns, remaining))).flatten
+    }
+  }
+
+  // Helper functions
+  /**
+   * Adds combination to existing List of combinations
+   */
+  private def combine[T](combinationToAdd: Combination[T], combinationsLists: List[List[Combination[T]]]): List[List[Combination[T]]] = {
+    combinationsLists.map(cl => combinationToAdd :: cl)
+  }
+
+  /**
+   * Take all combinations of group of size 'n' from source list 'l'.
+   * n should be <= l
+   * @param n  size of the combination
+   * @param l  source list
+   * @return List of tuples where ._1 is combination, _2 is the rest from input list 'l'
+   */
+  private def takeCombinations[T](n: Int, l: List[T]): List[(Combination[T], List[T])] = {
+    assert(n <= l.size, s"cannot takeCombination where N($n) > list.size(${l.size}")
+
+    val combinations = l.combinations(n).toList
+
+    combinations.map { c =>
+      val remaining = l.filterNot(c.contains(_))
+      (c, remaining)
+    }
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P28Spec.scala
+++ src/test/scala/com/robo/p99/scala/P28Spec.scala
@@ -1,10 +1,49 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+/**
+ * P28 (medium)
+You need to do P27 first.
 
+Generalize the predicate from P27 in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+
+Example:
+scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo, Ida)), ...
+
+Note that we do not want permutations of the group members; i.e. ((Aldo, Beat), ...) is the same solution as ((Beat, Aldo), ...).
+However, we make a difference between ((Aldo, Beat), (Carla, David), ...) and ((Carla, David), (Aldo, Beat), ...).
+You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
+
+ */
 class P28Spec extends UnitSpec {
 
-  // TODO
+  val list = List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+
+  "group" should "Group the elements of a set into disjoint subsets - easy case" in {
+    assert(P28.group(List(1, 2), List('A,'B,'C)) === List(List(List('A), List('B,'C)),List(List('B), List('A,'C)),List(List('C), List('A,'B))))
+  }
+
+  "group" should "Group the elements of a set into disjoint subsets - medium case " in {
+    assert(P28.group(List(2, 2), List('A,'B,'C, 'D)) === List(List(List('A,'B), List('C,'D)),
+                                                                   List(List('A, 'C), List('B,'D)),
+                                                                   List(List('A, 'D), List('B,'C)),
+
+                                                                   List(List('B, 'C), List('A,'D)),
+                                                                   List(List('B, 'D), List('A,'C)),
+
+                                                                   List(List('C, 'D), List('A,'B))
+    ))
+  }
+
+  "group" should "Group the elements as the P27.group3 function" in {
+    assert(P28.group(List(2, 3, 4), list).size === P27.group3(list).size)
+  }
+
+
+  "group" should "Group the elements as the P27.group3 function, strict " in {
+    assert(P28.group(List(2, 3, 4), list) === P27.group3(list))
+  }
 
 }
 
