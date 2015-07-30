cf86461934506034c96e9f31ade24c135a38d4ba
Rafał Wolak <RWolak@advaoptical.com>
2015-07-30 15:02:57 +0200

    P31 (medium): Determine whether a given integer number is prime.

--- src/main/scala/com/robo/p99/scala/P31.scala
+++ src/main/scala/com/robo/p99/scala/P31.scala
@@ -1,5 +1,13 @@
 package com.robo.p99.scala
 
+/**
+ * P31 (medium)
+Determine whether a given integer number is prime.
+
+Example:
+scala> isPrime(7)
+res0: Boolean = true
+ */
 object P31 {
 
   // TODO
