eca770aa51cfec7fefa9f19ebd9b4adca095b9b3
Rafał Wolak <RWolak@advaoptical.com>
2015-08-03 00:56:45 +0200

    P33 (easy) Determine whether two positive integer numbers are coprime. Two numbers are coprime if their greatest common divisor equals 1.

--- src/main/scala/com/robo/p99/scala/P33.scala
+++ src/main/scala/com/robo/p99/scala/P33.scala
@@ -1,7 +1,16 @@
 package com.robo.p99.scala
 
+/**
+ * P33 (easy)
+Determine whether two positive integer numbers are coprime.
+Two numbers are coprime if their greatest common divisor equals 1.
+
+Example:
+scala> isCoprimeTo(35, 64)
+res0: Boolean = true
+ */
 object P33 {
 
-  // TODO
+  def isCoprimeTo(a: Int, b: Int): Boolean = P32.gcd(a, b) == 1
 
 }
--- src/test/scala/com/robo/p99/scala/P33Spec.scala
+++ src/test/scala/com/robo/p99/scala/P33Spec.scala
@@ -2,9 +2,24 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P33 (easy)
+Determine whether two positive integer numbers are coprime.
+Two numbers are coprime if their greatest common divisor equals 1.
+
+Example:
+scala> isCoprimeTo(35, 64)
+res0: Boolean = true
+ */
 class P33Spec extends UnitSpec {
 
-  // TODO
+  "isCoprimeTo(35, 64)" should "retrun true" in {
+    assert(P33.isCoprimeTo(35, 64) === true)
+  }
+
+  "isCoprimeTo(9, 18)" should "retrun false" in {
+    assert(P33.isCoprimeTo(9, 18) === false)
+  }
 
 }
 
