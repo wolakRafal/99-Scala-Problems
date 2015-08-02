0e1f515122da3d39ad99cb8b89539393fc7dd4a5
Rafał Wolak <RWolak@advaoptical.com>
2015-08-02 22:05:24 +0200

    P32 (medium) Euclid's algorithm. Determine the greatest common divisor of two positive integer numbers.

--- src/main/scala/com/robo/p99/scala/P32.scala
+++ src/main/scala/com/robo/p99/scala/P32.scala
@@ -1,7 +1,18 @@
 package com.robo.p99.scala
 
+/**
+ * P32 (medium)
+    Determine the greatest common divisor of two positive integer numbers. Use Euclid's algorithm.
+
+Example:
+scala> gcd(36, 63)
+res0: Int = 9
+ */
 object P32 {
 
-  // TODO
+  def gcd(a : Int, b: Int): Int = b match {
+      case 0 => a
+      case _ => gcd(b,a % b)
+    }
 
 }
--- src/test/scala/com/robo/p99/scala/P32Spec.scala
+++ src/test/scala/com/robo/p99/scala/P32Spec.scala
@@ -2,9 +2,22 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P32 (medium)
+    Determine the greatest common divisor of two positive integer numbers. Use Euclid's algorithm.
+
+Example:
+scala> gcd(36, 63)
+res0: Int = 9
+ */
 class P32Spec extends UnitSpec {
 
-  // TODO
+  "gcd" should "Determine the greatest common divisor of two positive integer numbers" in {
+    assert(P32.gcd(36,63) === 9)
+  }
 
+  it should " 10 and 100" in {
+    assert(P32.gcd(10,100) === 10)
+  }
 }
 
