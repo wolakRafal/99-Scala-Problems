900acfd1f8d47dc5a6599b10bae6668a6ff98910
Rafał Wolak <RWolak@advaoptical.com>
2015-08-04 16:24:36 +0200

    P37 (medium): Calculate Euler's totient function phi(m) (improved).

--- src/test/scala/com/robo/p99/scala/P37Spec.scala
+++ src/test/scala/com/robo/p99/scala/P37Spec.scala
@@ -4,7 +4,17 @@ import org.p99.scala.UnitSpec
 
 class P37Spec extends UnitSpec {
 
-  // TODO
+  "phi(10)" should "Calculate Euler's totient function phi(m)" in {
+    assert(P37.phi(10) === 4)
+  }
+
+  //  n     	1	2	3	4	5	6	7	8	9	10 11	12
+  //  phi(n)	1	1	2	2	4	2	6	4	6	4	10	4
+  "phi(2)" should " return 1" in {assert(P37.phi(2) === 1)}
+  "phi(3)" should " return 2" in {assert(P37.phi(3) === 2)}
+  "phi(5)" should " return 4" in {assert(P37.phi(5) === 4)}
+  "phi(6)" should " return 2" in {assert(P37.phi(6) === 2)}
+
 
 }
 
