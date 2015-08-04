ba9bd27b4da7b01b1d227f5e48991fe386ac9e57
Rafał Wolak <RWolak@advaoptical.com>
2015-08-04 14:30:15 +0200

    P34 (medium): Calculate Euler's totient function phi(m).

--- src/main/scala/com/robo/p99/scala/P34.scala
+++ src/main/scala/com/robo/p99/scala/P34.scala
@@ -1,7 +1,15 @@
 package com.robo.p99.scala
 
+/**
+ * P34 (medium)
+Calculate Euler's totient function phi(m). Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r <= m) that are coprime to m.
+
+Example:
+scala> totient(10)
+res0: Int = 4
+ */
 object P34 {
 
-  // TODO
+  def totient(m: Int) : Int = (1 to m).count(r => P33.isCoprimeTo(r, m))
 
 }
--- src/test/scala/com/robo/p99/scala/P34Spec.scala
+++ src/test/scala/com/robo/p99/scala/P34Spec.scala
@@ -2,9 +2,27 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P34 (medium)
+Calculate Euler's totient function phi(m). Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r <= m) that are coprime to m.
+
+Example:
+scala> totient(10)
+res0: Int = 4
+ */
 class P34Spec extends UnitSpec {
 
-  // TODO
+  "totient" should "Calculate Euler's totient function phi(m)" in {
+    assert(P34.totient(10) === 4)
+  }
+
+//  n     	1	2	3	4	5	6	7	8	9	10 11	12
+//  phi(n)	1	1	2	2	4	2	6	4	6	4	10	4
+  "phi(1)" should " return 1" in {assert(P34.totient(1) === 1)}
+  "phi(2)" should " return 1" in {assert(P34.totient(2) === 1)}
+  "phi(3)" should " return 2" in {assert(P34.totient(3) === 2)}
+  "phi(5)" should " return 4" in {assert(P34.totient(5) === 4)}
+  "phi(6)" should " return 2" in {assert(P34.totient(6) === 2)}
 
 }
 
