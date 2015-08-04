bb3e12d155586b953bd9902cabeba646284d4ad5
Rafał Wolak <RWolak@advaoptical.com>
2015-08-04 16:07:47 +0200

    P36 (medium): Determine the prime factors of a given positive integer (2).   Construct a list containing the prime factors and their multiplicity.

--- src/main/scala/com/robo/p99/scala/P36.scala
+++ src/main/scala/com/robo/p99/scala/P36.scala
@@ -1,7 +1,24 @@
 package com.robo.p99.scala
 
+/**
+ * P36 (medium)
+
+Determine the prime factors of a given positive integer (2). Construct a list
+containing the prime factors and their multiplicity.
+Example:
+
+    scala> primeFactorMultiplicity(315)
+    res0: Map[Int,Int] = Map(3 -> 2, 5 -> 1, 7 -> 1)
+ */
 object P36 {
 
-  // TODO
+  def primeFactorMultiplicity(n : Int): Map[Int, Int] = {
+    val grouped: Map[Int, List[Int]] = P35.primeFactors(n).groupBy(identity)
+    grouped.map{case (a, listA) => (a , listA.size)}
+  }
 
+  def primeFactorMultiplicity_(n: Int): Map[Int, Int] = {
+    val grouped: Map[Int, List[Int]] = P35.primeFactors(n).groupBy(identity)
+    grouped.map(a => (a._1, a._2.size))
+  }
 }
--- src/test/scala/com/robo/p99/scala/P36Spec.scala
+++ src/test/scala/com/robo/p99/scala/P36Spec.scala
@@ -2,9 +2,22 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+
+/**
+ * P36 (medium)
+
+Determine the prime factors of a given positive integer (2). Construct a list
+containing the prime factors and their multiplicity.
+Example:
+
+    scala> primeFactorMultiplicity(315)
+    res0: Map[Int,Int] = Map(3 -> 2, 5 -> 1, 7 -> 1)
+ */
 class P36Spec extends UnitSpec {
 
-  // TODO
+  "primeFactorMultiplicity(315)" should "return " in {
+    assert(P36.primeFactorMultiplicity(315) === Map(3 -> 2, 5 -> 1, 7 -> 1))
+  }
 
 }
 
