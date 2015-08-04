80d46dec44500cc9681b65c2330e518247d59d6e
Rafał Wolak <RWolak@advaoptical.com>
2015-08-04 15:37:19 +0200

    P35 (medium) Determine the prime factors of a given positive integer.

--- src/main/scala/com/robo/p99/scala/P35.scala
+++ src/main/scala/com/robo/p99/scala/P35.scala
@@ -1,7 +1,29 @@
 package com.robo.p99.scala
 
+/**
+ * P35 (medium)
+  Determine the prime factors of a given positive integer.
+  Construct a flat list containing the prime factors in ascending order.
+
+Example:
+scala> primeFactors(315)
+res0: List[Int] = List(3, 3, 5, 7)
+ */
 object P35 {
 
-  // TODO
+
+  /** Generates primes from 2 to N*/
+  private def primeGen(n: Int): Stream[Int] = P31.sieve(2, n)
+
+  private def primeFactorsRec(n: Int, primeFactors: List[Int]): List[Int] = {
+    if(P31.isPrime(n)) {
+      n :: primeFactors
+    } else {
+      val primeFactor = primeGen(n).find(m => (n % m) == 0).get
+      primeFactorsRec(n / primeFactor, primeFactor :: primeFactors)
+    }
+  }
+
+  def primeFactors(n: Int): List[Int] = primeFactorsRec(n, List()).sorted
 
 }
--- src/test/scala/com/robo/p99/scala/P35Spec.scala
+++ src/test/scala/com/robo/p99/scala/P35Spec.scala
@@ -2,9 +2,28 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P35 (medium)
+  Determine the prime factors of a given positive integer.
+  Construct a flat list containing the prime factors in ascending order.
+
+Example:
+scala> primeFactors(315)
+res0: List[Int] = List(3, 3, 5, 7)
+ */
 class P35Spec extends UnitSpec {
 
-  // TODO
+  "primeFactors(315)" should "return List(3, 3, 5, 7)" in {
+    assert(P35.primeFactors(315) === List(3, 3, 5, 7))
+  }
+
+  "primeFactors(12)" should "return List(2, 2, 3)" in {
+    assert(P35.primeFactors(12) === List(2, 2, 3))
+  }
+
+  "primeFactors(2)" should "return List(2)" in {
+    assert(P35.primeFactors(2) === List(2))
+  }
 
 }
 
