020390b9dcf86d88e198401a1bc6715d2a8bfd72
Rafał Wolak <RWolak@advaoptical.com>
2015-08-07 16:55:17 +0200

    P39 (easy) A list of prime numbers. Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.

--- src/main/scala/com/robo/p99/scala/P39.scala
+++ src/main/scala/com/robo/p99/scala/P39.scala
@@ -1,7 +1,22 @@
 package com.robo.p99.scala
 
+/**
+ * P39 (easy)
+A list of prime numbers. Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+
+Example:
+scala> listPrimesinRange(7 to 31)
+res0: List[Int] = List(7, 11, 13, 17, 19, 23, 29, 31)
+ */
 object P39 {
 
-  // TODO
+  def listPrimesInRange(range: Range) = {
+    range.filter(P31.isPrime)
+  }
+
+  /** Used fast but probabilistic method, and the filter using isPrime for small vector */
+  def listPrimesInRangeFast(range: Range) = {
+    range.filter(P31.isPseudoPrime)
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P39Spec.scala
+++ src/test/scala/com/robo/p99/scala/P39Spec.scala
@@ -2,9 +2,40 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P39 (easy)
+A list of prime numbers. Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+
+Example:
+scala> listPrimesinRange(7 to 31)
+res0: List[Int] = List(7, 11, 13, 17, 19, 23, 29, 31)
+ */
 class P39Spec extends UnitSpec {
 
-  // TODO
+  "listPrimesinRange" should "construct a list of all prime numbers in range" in {
+    assert(P39.listPrimesInRange(7 to 31) === List(7, 11, 13, 17, 19, 23, 29, 31))
+  }
+
+  "listPrimesinRangeFast" should "construct a list of all prime numbers in range" in {
+    assert(P39.listPrimesInRangeFast(7 to 31) === List(7, 11, 13, 17, 19, 23, 29, 31))
+  }
+
+  "Compare listPrimesInRange" should " " in {
+
+    val startSlow = System.currentTimeMillis
+    val slowSize = P39.listPrimesInRange(2 to 100000).size
+    val stopSlow = System.currentTimeMillis
+
+    val start = System.currentTimeMillis
+    val fastSize = P39.listPrimesInRangeFast(2 to 100000).size
+    val stop = System.currentTimeMillis
+
+    val slowTime = stopSlow - startSlow
+    val fastTime = stop - start
+    println(s"For Range 2 to 100000. Slower function takes $slowTime ms to calculate $slowSize. Faster takes $fastTime ms to calculate $fastSize" )
+    assert(slowTime < fastTime)
+  }
+
 
 }
 
