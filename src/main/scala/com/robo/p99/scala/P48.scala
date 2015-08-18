56c4d3f4e103a02f03ca17f00e20d708456ae1bb
Rafał Wolak <RWolak@advaoptical.com>
2015-08-18 17:30:29 +0200

    P48 (easy) Contination of problem P46 by redefining and, or, etc as operators. Using implicits (i.e. make them methods of a new class with an implicit conversion from Boolean.) not will have to be left as a object method.

--- src/main/scala/com/robo/p99/scala/P48.scala
+++ src/main/scala/com/robo/p99/scala/P48.scala
@@ -1,7 +1,27 @@
 package com.robo.p99.scala
+/**
+ * P48 (easy)
+Continue problem P46 by redefining and, or, etc as operators.
+(i.e. make them methods of a new class with an implicit conversion from Boolean.) not will have to be left as a object method.
 
+scala> table2((a: Boolean, b: Boolean) => a and (a or not(b)))
+A     B     result
+true  true  true
+true  false true
+false true  false
+false false false
+ */
 object P48 {
 
-  // TODO
+  implicit class BooleanOperators(thiz: Boolean) {
+    def and(that: Boolean): Boolean = P46.and(thiz, that)
+    def or(that: Boolean): Boolean = P46.or(thiz, that)
+    def nand(that: Boolean): Boolean = P46.nand(thiz, that)
+    def nor(that: Boolean): Boolean = P46.nor(thiz, that)
+    def xor(that: Boolean): Boolean = P46.xor(thiz, that)
+    def equ(that: Boolean): Boolean = P46.equ(thiz, that)
+    def impl(that: Boolean): Boolean = P46.impl(thiz, that)
+  }
 
+  def not(b: Boolean) = !b
 }
--- src/test/scala/com/robo/p99/scala/P48Spec.scala
+++ src/test/scala/com/robo/p99/scala/P48Spec.scala
@@ -1,10 +1,25 @@
 package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
+import P48._
 
+/**
+ * P48 (easy)
+Continue problem P46 by redefining and, or, etc as operators.
+(i.e. make them methods of a new class with an implicit conversion from Boolean.) not will have to be left as a object method.
+
+scala> table2((a: Boolean, b: Boolean) => a and (a or not(b)))
+A     B     result
+true  true  true
+true  false true
+false true  false
+false false false
+ */
 class P48Spec extends UnitSpec {
 
-  // TODO
+  "P48, Operators" should "print truth table for boolean operators" in {
+    P47.table2((a: Boolean, b: Boolean) => a and (a or not(b)))
+  }
 
 }
 
