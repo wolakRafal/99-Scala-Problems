75771965c1801016c7477cd1a398dc46f77ae6f7
Rafał Wolak <RWolak@advaoptical.com>
2015-08-18 09:04:53 +0200

    P97 : Sudoku first try

--- src/main/scala/com/robo/p99/scala/P97.scala
+++ src/main/scala/com/robo/p99/scala/P97.scala
@@ -1,5 +1,7 @@
 package com.robo.p99.scala
 
+import scala.collection.immutable.::
+
 /**
  * P97 (medium)
 Sudoku puzzles go like this:
@@ -22,7 +24,7 @@ Problem statement Solution
         |         |                      |         |
 2  4  . | .  .  1 | 5  .  .      2  4  6 | 3  9  1 | 5  7  8
 Every spot in the puzzle belongs to a (horizontal) row and a (vertical) column,
-as well as to one single 3×3 square (which we call "square" for short). At
+as well as to one single 3ï¿½3 square (which we call "square" for short). At
 the beginning, some of the spots carry a single-digit number between 1 and 9.
 The problem is to fill the missing spots with digits in such a way that every number between 1 and 9 appears
 exactly once in each row, in each column, and in each square.
@@ -35,58 +37,95 @@ object P97 {
 
   type SudokuPuzzle = List[Row]
 
-  def sudoku(inputPuzzle: SudokuPuzzle): SudokuPuzzle = sudokuRec(inputPuzzle).get
+  def sudoku(inputPuzzle: SudokuPuzzle): SudokuPuzzle = {
+    println("inputPuzzle:" + inputPuzzle.mkString("\n"))
+    sudokuRec(inputPuzzle, List.empty[SudokuPuzzle]).get
+  }
 
-  private def sudokuRec(sudoku: SudokuPuzzle): Option[SudokuPuzzle] = {
+  def sudokuRec(sudoku: SudokuPuzzle, visited: List[SudokuPuzzle]): Option[SudokuPuzzle] = {
+//    println("sudokuRec " + sudoku.map(_.count(_.v.isDefined)).sum)
     takeFirstEmptyField(sudoku) match {
-      case Some(point) => tryValues(possibleValues(point, sudoku), sudoku)
+      case Some(point) => tryValues(possibleValues(point, sudoku), sudoku, visited)
       case None => Some(sudoku) // Bingo we have a solution
     }
   }
 
   /**
-    * Recursively tries values from list l to obtain solution
-    */
-  private def tryValues(l: List[Point], s: SudokuPuzzle): Option[SudokuPuzzle] = {
-    l match {
-      case x :: xs =>
-        val solution = sudokuRec(updateSudoku(x, s)) // go deep
-        if (solution.isEmpty) {
-          tryValues(xs, s) // try next value
-        } else {
-          solution // we found solution
+   * Recursively tries values from list l to obtain solution
+   */
+  def tryValues(l: List[Point], s: SudokuPuzzle, visited: List[SudokuPuzzle]): Option[SudokuPuzzle] = {
+        println("tryValues l=" + l)
+
+    var result: Option[SudokuPuzzle] = None
+    var list = l
+
+    while (list.nonEmpty && result.isEmpty) {
+      val p = l.head
+      list = l.tail
+      println("list.tail" + list)
+      val newSolution = updateSudoku(p, s)
+      if (!visited.contains(newSolution)) {
+        val partialSolution = sudokuRec(newSolution,visited :+ newSolution)
+
+        println("partialSolution" + partialSolution)
+        if (partialSolution.isDefined) {
+          result = partialSolution
         }
-      case Nil => None // Dead End, try another branch
+      }
     }
+    result
+//
+//    l match {
+//      case x :: xs =>
+////        println("tryValues" + x + " :: " + xs)
+//        val solution = sudokuRec(updateSudoku(x, s)) // go deep
+////        println(s"tryValues, solution=$solution")
+//        if (solution.isEmpty) {
+//          tryValues(xs, s) // try next value
+//        } else {
+//          solution // we found solution
+//        }
+//      case Nil =>
+////        println("tryValues Dead End, try another branch")
+//        None // Dead End, try another branch
+//    }
   }
 
-  private def takeFirstEmptyField(s: SudokuPuzzle): Option[Point] =
+  def takeFirstEmptyField(s: SudokuPuzzle): Option[Point] =
     s.find(row => row.count(_.v.isEmpty) > 0).getOrElse(List()).find(_.v.isEmpty)
 
-  private def valuesInRow(row: Int, s: SudokuPuzzle): Set[Int] = s(row).filter(_.v.isDefined).map(_.v.get).toSet
+  def valuesInRow(row: Int, s: SudokuPuzzle): Set[Int] = s(row).filter(_.v.isDefined).map(_.v.get).toSet
 
-  private def valuesInCol(col: Int, s: SudokuPuzzle): Set[Int] = s.map(_(col)).filter(_.v.isDefined).map(_.v.get).toSet
+  def valuesInCol(col: Int, s: SudokuPuzzle): Set[Int] = s.map(_(col)).filter(_.v.isDefined).map(_.v.get).toSet
 
-  private def valuesInSquare(p: Point, s: SudokuPuzzle): Set[Int] = {
-    val t = squareMap(square(p))
-    val pointsInSquare = for (x <- t._1; y <- t._2) yield (x, y)
-    s.flatten.filter(p => pointsInSquare.contains((p.x, p.y))).filter(_.v.isDefined).map(_.v.get).toSet
+  def valuesInSquare(p: Point, s: SudokuPuzzle): Set[Int] = {
+//    println(s"valuesInSquare $p, s=$s")
+    val points = pointsInSquare(square(p))
+//    println(s"valuesInSquare points=$points")
+    val tmp = s.flatten.filter(p => points.contains((p.x, p.y)))
+//    println(s"valuesInSquare tmp=$tmp")
+    tmp.filter(_.v.isDefined).map(_.v.get).toSet
   }
 
-  private def possibleValues(p: Point, s: SudokuPuzzle): List[Point] = {
+  def pointsInSquare(square: Int): List[(Int, Int)] = {
+    val t = squareMap(square)
+    for (x <- t._1; y <- t._2) yield (x, y)
+  }
+
+  def possibleValues(p: Point, s: SudokuPuzzle): List[Point] = {
     var vals = Set[Int](1, 2, 3, 4, 5, 6, 7, 8, 9)
     vals = ((vals -- valuesInRow(p.x, s)) -- valuesInCol(p.y, s)) -- valuesInSquare(p, s)
     vals.toList.map(v => p.copy(v = Some(v)))
   }
 
-  private def updateSudoku(sv: Point, s: SudokuPuzzle): SudokuPuzzle = {
+  def updateSudoku(sv: Point, s: SudokuPuzzle): SudokuPuzzle = {
     val row = s(sv.x)
     val newRow = row.updated(sv.y, sv)
     s.updated(sv.x, newRow)
   }
 
   // Squares numbered from 1 to 9 counting from left upper corner
-  private def square(p: Point): Int = (p.x, p.y) match {
+  def square(p: Point): Int = (p.x, p.y) match {
     case (x,y) if x < 3 && y < 3 => 1
     case (x,y) if x < 6 && y < 3 => 2
     case (x,y) if x < 9 && y < 3 => 3
@@ -99,7 +138,7 @@ object P97 {
   }
 
   // key : square number (from 1 to 9), value: Tuple with row and col indices in 3x3 square
-  private val squareMap = Map(1 ->(List(0, 1, 2), List(0, 1, 2)),
+  val squareMap = Map(1 ->(List(0, 1, 2), List(0, 1, 2)),
     2 ->(List(3, 4, 5), List(0, 1, 2)),
     3 ->(List(6, 7, 8), List(0, 1, 2)),
 
--- src/test/scala/com/robo/p99/scala/P97Spec.scala
+++ src/test/scala/com/robo/p99/scala/P97Spec.scala
@@ -2,6 +2,7 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 import P97._
+import P97._
 
 /**
  * P97 (medium)
@@ -25,7 +26,7 @@ Problem statement Solution
         |         |                      |         |
 2  4  . | .  .  1 | 5  .  .      2  4  6 | 3  9  1 | 5  7  8
 Every spot in the puzzle belongs to a (horizontal) row and a (vertical) column,
-as well as to one single 3×3 square (which we call "square" for short). At
+as well as to one single 3ï¿½3 square (which we call "square" for short). At
 the beginning, some of the spots carry a single-digit number between 1 and 9.
 The problem is to fill the missing spots with digits in such a way that every number between 1 and 9 appears
 exactly once in each row, in each column, and in each square.
@@ -50,14 +51,100 @@ class P97Spec extends UnitSpec {
                  |        |         |
                  |2  4  . | .  .  1 | 5  .  .""".stripMargin
 
-  def parse(inputString: String): SudokuPuzzle = ???
+
+  val puzzleSmall =
+               """.  .  4
+                 |
+                 |6  7  .
+                 |
+                 |5  .  8""".stripMargin
+
+
+  def even(n:Int) = n % 2 == 0
+
+  def parse(inputString: String): SudokuPuzzle = {
+    //    TODO  eliminate odd lines numbers 1,3,5,7...
+    val linesWithNumbers: List[(String, Int)] = inputString.lines.zipWithIndex.filter(t => even(t._2)).map(_._1).zipWithIndex.toList
+    linesWithNumbers.map { case (line, x) =>
+      val chars = line.filter(c => !(c.equals(' ') || c.equals('|'))).toList // eliminate spaces and '|'
+
+      chars.zipWithIndex.map { case (c, y) =>
+        if (c.equals('.')) {
+          Point(x, y, None)
+        } else {
+          Point(x, y, Some(c.toString.toInt))
+        }
+      }
+    }
+  }
 
   val input : SudokuPuzzle = parse(puzzle)
 
-  val excpectedSolution = ???
+//  val excpectedSolution = ???
+
+  "takeFirstEmptyField" should "return (0,0)" in {
+    assert(takeFirstEmptyField(parse(puzzle)) === Some(Point(0, 0, None)))
+  }
+
+  "valuesInRow" should "work" in {
+    assert(valuesInRow(4, parse(puzzle)) === Set(6, 9, 7, 8))
+  }
+
+  "valuesInCol" should "work" in {
+    assert(valuesInCol(4, parse(puzzle)) === Set(3, 4, 6, 8))
+  }
+
+  "square" should "work" in {
+    assert(square(Point(0, 0, None)) === 1)
+    assert(square(Point(4, 3, None)) === 5)
+  }
+
+  "pointsInSquare 1" should "return " in {
+    assert(pointsInSquare(1).toSet === List(  (0,0), (0,1), (0,2),
+                                        (1,0), (1,1), (1,2),
+                                        (2,0), (2,1), (2,2)
+    ).toSet)
+  }
+
+  "possibleValues" should "return " in {
+    assert(possibleValues(Point(0, 0, None), parse(puzzle)).map(_.v.get).toSet === Set(9))
+  }
+
+  val expectedPuzzle =  """9  .  4 | 8  .  . | .  1  7
+                          |        |         |
+                          |6  7  . | 9  .  . | .  .  .
+                          |        |         |
+                          |5  .  8 | .  3  . | .  .  4
+                          |--------+---------+--------
+                          |3  .  . | 7  4  . | 1  .  .
+                          |        |         |
+                          |.  6  9 | .  .  . | 7  8  .
+                          |        |         |
+                          |.  .  1 | .  6  9 | .  .  5
+                          |--------+---------+--------
+                          |1  .  . | .  8  . | 3  .  6
+                          |        |         |
+                          |.  .  . | .  .  6 | .  9  1
+                          |        |         |
+                          |2  4  . | .  .  1 | 5  .  .""".stripMargin
+
+
+  "updateSudoku" should "return " in {
+    assert(updateSudoku(Point(0, 0, Some(9)), parse(puzzle)) === parse(expectedPuzzle))
+  }
+
+
+  "valuesInSquare 1 " should "return Set(4, 6, 7,5,8)" in {
+    assert(valuesInSquare(Point(0,0,None), parse(puzzle)) === Set(4, 6, 7,5,8))
+  }
+
+  "valuesInSquare 5 " should "return Set(7, 4,6,9)" in {
+    assert(valuesInSquare(Point(4, 3, None), parse(puzzle)) === Set(7, 4, 6, 9))
+  }
+
 
   "sudoku" should "find a solution for given example" in {
-
+    println(P97.sudoku(input).mkString("\n"))
   }
 
 }
