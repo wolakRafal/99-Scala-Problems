cd4f41966723f09e29e77e365a25b6e712aa74e1
Rafał Wolak <RWolak@advaoptical.com>
2015-08-19 09:10:46 +0200

    P49 (medium) Find out the construction rules and write a function to generate Gray codes.

--- src/main/scala/com/robo/p99/scala/P49.scala
+++ src/main/scala/com/robo/p99/scala/P49.scala
@@ -1,7 +1,28 @@
 package com.robo.p99.scala
 
+/**
+ * P49 (medium)
+An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example:
+n = 1: C(1) = ("0", "1").
+n = 2: C(2) = ("00", "01", "11", "10").
+n = 3: C(3) = ("000", "001", "011", "010", "110", "111", "101", "100").
+
+Find out the construction rules and write a function to generate Gray codes.
+
+Example:
+scala> gray(3)
+res0 List[String] = List(000, 001, 011, 010, 110, 111, 101, 100)
+See if you can use memoization to make the function more efficient.
+
+ */
 object P49 {
 
-  // TODO
+  def gray(n: Int): List[String] = {
+    var gCodes = Vector("0", "1")
+    2 to n foreach { x =>
+      gCodes = gCodes.map("0" + _ ) ++ gCodes.reverseMap("1" + _ )
+    }
+    gCodes.toList
+  }
 
 }
--- src/test/scala/com/robo/p99/scala/P49Spec.scala
+++ src/test/scala/com/robo/p99/scala/P49Spec.scala
@@ -2,9 +2,27 @@ package com.robo.p99.scala
 
 import org.p99.scala.UnitSpec
 
+/**
+ * P49 (medium)
+An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example:
+n = 1: C(1) = ("0", "1").
+n = 2: C(2) = ("00", "01", "11", "10").
+n = 3: C(3) = ("000", "001", "011", "010", "110", "111", "101", "100").
+
+Find out the construction rules and write a function to generate Gray codes.
+
+Example:
+scala> gray(3)
+res0 List[String] = List(000, 001, 011, 010, 110, 111, 101, 100)
+See if you can use memoization to make the function more efficient.
+
+ */
+
 class P49Spec extends UnitSpec {
 
-  // TODO
+  "gray(3)" should "generate List(000, 001, 011, 010, 110, 111, 101, 100)" in {
+    assert(P49.gray(3) === List("000", "001", "011", "010", "110", "111", "101", "100"))
+  }
 
 }
 
